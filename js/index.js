c33d12af3d5304b348a71db0f717679452ab2b4f
wanglintc <1587238460@qq.com>
2022-05-05 17:45:31 +0800

    0505的第2次

--- js/index.js
+++ js/index.js
@@ -71,7 +71,65 @@ class Index{
         this.autoPlay();
     }
     //倒计时的实现
-    theCountdownFn(){
+    async theCountdownFn(){
+        let times; 
+        //获取4个节点  在定时器里面获取报错
+        let zhengdian = this.$('.zhengdian');
+        let hour = this.$('.hour');
+        let minute = this.$('.minute');
+        let second = this.$('.second');
+        
+        // 开启个定时器
+        times = setInterval(function () {
+        //获取第一个当前的时间
+        let date1 = new Date;
+        //获取第一个当前时间的小时数
+        date1 = date1.getHours();
+        // console.log(date1);
+        //如果时间是偶数 就+2
+        if (date1 %2==0) {
+            date1+=2;
+            // console.log(date1);
+        }else{
+            //是奇数  +1
+            date1+=1;
+            // console.log(date1);
+        }
+        //此时的date1是每隔2小时的时间
+        //把他变成时间戳
+        //把他变成字符串 追加到页面中
+        let param = date1+':00';
+        zhengdian.innerHTML = param;
+        //把第一个时间变成时间戳
+        let a = date1*1000*60*60;
+        // console.log(a);
+        //获取第二个当前时间  把他的年月日变成2022年5月5号
+        let date2= new Date;
+        date2.setFullYear(2022);
+        date2.setMonth(4);
+        //此时的date2为5.5的现在时间的时间戳
+        date2.setDate(5);
+        date2 = date2 -0;
+        //console.log(date2);
+        //获取5.5的0时0分0秒的时间戳  
+        let date3 =new Date(2022,4,5);
+        // console.log(date3);
+        //获取5月5号现在几点的时间戳 - 5月5号的0时0分的时间戳
+        let b= date2 - date3;
+        // console.log(b);
+        // console.log(a-b);
+        //此时的c为倒计时的时间戳
+        let c = a-b;
+        // console.log(a,b,c);
+        // 把c 变成一个秒数
+        
+        c = Math.round(c/1000);
+        console.log(c/60);
+            hour.innerHTML = Math.floor(c/(60*60));
+            minute.innerHTML = Math.floor(c/60-Math.floor(c/(60*60))*60);
+            second.innerHTML = c%60;
+        },1000)
+        
         
     }
 
--- js/pay.js
+++ js/pay.js
@@ -4,20 +4,35 @@ class Pay{
         this.getData();
     }
     getData(){
+        //判断打开结算页面  如果未携带参数就跳回到购物车页面
+        
+        if (!location.search) {
+            //未携带参数
+            location.assign('./cart.html');
+        }else{
+            //携带参数
         //获取用户id
         let unama = localStorage.getItem('user_id');
-        //获取商品id
+        //获取商品id   此时的id是字符串  
         let goodsId =location.search;
         // console.log(goodsId);
-        goodsId = goodsId.substring(1,goodsId.length)
+        goodsId = goodsId.substring(1,goodsId.length);
+        //把字符串改成数组
         goodsId = goodsId.split(',');
         // console.log(goodsId);
+        
+
+
         //发送请求获取商品的详细信息 因为async有就近原则
+        //循环数组  
         goodsId.forEach(val => {
             // console.log(val);
+            //传的参数是id值
             this.axiosFn(val);
         });
         
+        }
+      
        
     }
     async axiosFn(a){
